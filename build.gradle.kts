5dcfc54410032b7fbc4fa7a0235a071877793203
notifybibi <notifybibi@qq.com>
2023-09-28 17:10:06 +0800

    release community version 2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val guavaVersion: String by rootProject
 val semVer: String? by project
 val configDir by extra { file("$buildDir/analysis-config") }
 val pluginDir by extra { file("$buildDir/analysis-config/plugins") }
-version = semVer ?: "2.0-SNAPSHOT"
+version = semVer ?: "2.2"
 
 plugins {
     `java-library`
